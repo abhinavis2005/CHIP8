8cf682854d254f572ef9d4304c6331d1f2642c13
pappan12 <apdv8002@gmail.com>
2025-01-03 14:19:43 +0530

    added file input and load_to_ram()

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -3,6 +3,7 @@ pub const SCREEN_HEIGHT: usize = 32;
 pub const STACK_SIZE: usize = 16;
 
 const RAM_SIZE: usize = 4096;
+const START_ADDRESS: usize = 0x200;
 const NUM_REGISTERS: usize = 16;
 pub struct Emulator {
     pc: u16,
@@ -12,4 +13,27 @@ pub struct Emulator {
     delay_timer: u8,
     sound_timer: u8,
     stack: [u16; STACK_SIZE],
+}
+
+impl Emulator {
+    pub fn new() -> Self {
+        Emulator {
+            pc: 0,
+            ram: [0; RAM_SIZE],
+            v: [0; NUM_REGISTERS],
+            i: 0,
+            delay_timer: 0,
+            sound_timer: 0,
+            stack: [0; STACK_SIZE],
+        }
+    }
+
+    //Loading the opcodes to RAM
+    pub fn load_to_ram(&mut self, opcodes: &[u8]) {
+        let end_address = START_ADDRESS + opcodes.len();
+        if end_address > self.ram.len() {
+            panic!("RAM size exceeded");
+        }
+        self.ram[START_ADDRESS..end_address].copy_from_slice(opcodes);
+    }
 }
--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,3 +1,17 @@
+use chip8_core::Emulator;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    // Receiving the ROM file from argument
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: cargo run [full_path_to_game_file]");
+        process::exit(1);
+    }
+    let buffer = fs::read(&args[1]).expect("Error reading the file");
+
+    // Emulator Initialisation and loading ROM into RAM
+    let mut chip8 = Emulator::new();
+    chip8.load_to_ram(&buffer);
 }
